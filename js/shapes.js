504a5f7c49f1a40f23471846cd57abfa4e801b0e
mcoppola <mcoppola832@gmail.com>
2015-03-05 08:48:08 -0500

    added rectang shape and super cool rectang demo

--- js/shapes.js
+++ js/shapes.js
@@ -6,41 +6,57 @@ function Shapes() {}
 
 
 Shapes.prototype.cube = function(len, char, color) {
-	if (char === undefined) { char = '-'}
+	if (char === undefined) { char = '.'}
 	if (color === undefined) { color = '#000'}
 	var model = [];
 
-	for (var j=0; j < len; j++) {
+	for (var i=0; i < len; i++) {
 		/// ------------------------------ //
 		// FRONT                           //
 		// ------------------------------- //
 		// left, right, bottom, top
-		model.push([0,j,0, char, color]);
-		model.push([len,j,0, char, color]);
-		model.push([j,0,0, char, color]);
-		model.push([j,len,0, char, color]);
+		model.push([0,i,0, char, color]);
+		model.push([len,i,0, char, color]);
+		model.push([i,0,0, char, color]);
+		model.push([i,len,0, char, color]);
 
 		// ------------------------------- //
 		// BACK                            //
 		// ------------------------------- //
 		// left, right, bottom, top
-		model.push([0,j,len, char, color]);
-		model.push([len,j,len, char, color]);
-		model.push([j,0,len, char, color]);
-		model.push([j,len,len, char, color]);
+		model.push([0,i,len, char, color]);
+		model.push([len,i,len, char, color]);
+		model.push([i,0,len, char, color]);
+		model.push([i,len,len, char, color]);
 
 		// ------------------------------- //
 		// SIDES                           //
 		// ------------------------------- //
 		// btm left, top left, btm right, top right
-		model.push([0,0,j, char, color]);
-		model.push([0,len,j, char, color]);
-		model.push([len,0,j, char, color]);
-		model.push([len,len,j, char, color]);
+		model.push([0,0,i, char, color]);
+		model.push([0,len,i, char, color]);
+		model.push([len,0,i, char, color]);
+		model.push([len,len,i, char, color]);
 
 	}	
 	return model;
-}	
+}
+
+Shapes.prototype.rectang = function(w, h, char, color) {
+	if (char === undefined) { char = '.'}
+	if (color === undefined) { color = '#000'}
+	var model = [];
+
+	for (var i=0; i < w; i++) {
+		model.push([i,0,0, char, color]);
+		model.push([i,h,0, char, color]);
+	}
+	for (var j=0; j < h+1; j++) {
+		model.push([0,j,0, char, color]);
+		model.push([w,j,0, char, color]);
+	}
+	return model;
+}
 
 // 2D grid
 Shapes.prototype.grid = function ( width, height, asci, color ) {
--- test/script.js
+++ test/script.js
@@ -15,18 +15,25 @@ window.onload = function () {
     // --------------- MAKE ASSETS ------------------------------------------------------- //
     
 
-    var cube = new Asset(envi.shapes.cube(20, '#'), window.innerWidth/2 - 150, window.innerHeight/2 - 150, 25, 15);
-    var inner = new Asset(envi.shapes.cube(20, '*', "#f00"), window.innerWidth/2 - 100, window.innerHeight/2 - 100, 100, 10);
-    var center = new Asset(envi.shapes.cube(10, '.', "#ff0"), window.innerWidth/2 - 50, window.innerHeight/2 - 50, 150, 10);
+    var recA = new Asset(envi.shapes.rectang(30,10, '@', '#f0e'), window.innerWidth/2 - 450, window.innerHeight/2 - 150, 100, 30);
+    var recB = new Asset(envi.shapes.rectang(30,10, '@', '#0fe'), window.innerWidth/2 - 450, window.innerHeight/2 - 150, 200, 30);
+    var recC = new Asset(envi.shapes.rectang(30,10, '@', '#fa0'), window.innerWidth/2 - 450, window.innerHeight/2 - 150, 300, 30);
+    
+    var cubeA = new Asset(envi.shapes.cube(20, '*', '#adf'), window.innerWidth/2 - 450, window.innerHeight/2 - 150, 400, 15);
+    var cubeB = new Asset(envi.shapes.cube(20, '*', '#adf'), window.innerWidth/2 - 150, window.innerHeight/2 - 150, 400, 15);
+    var cubeC = new Asset(envi.shapes.cube(20, '*', '#adf'), window.innerWidth/2 + 150, window.innerHeight/2 - 150, 400, 15);
 
 
     // --------------- LOAD ASSETS ------------------------------------------------------- //
 
     var assets = [];
-    
-    assets.push(cube);
-    assets.push(inner);
-    assets.push(center);
+
+    assets.push(recA);
+    assets.push(recB);
+    assets.push(recC);
+    assets.push(cubeA);
+    assets.push(cubeB);
+    assets.push(cubeC);
 
 
     // --------------- MAKE SCENE ------------------------------------------------------- //
