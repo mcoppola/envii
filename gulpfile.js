a5537ec0779798c8a8cc4c9d15301b731b65577d
Matthew Coppola <mcoppola832@gmail.com>
2016-10-23 15:30:33 -0400

    updated gulp flow with watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+
 
 gulp.task('build', function () {
   gulp.src(['./js/envii.js', './js/!(envii)*.js'])
     .pipe(uglify())
-    .pipe(concat('envii.min.js'))
+    .pipe(concat('envii/min.js'))
     .pipe(gulp.dest('./build'));
 });
 
+gulp.task('bundle-js', function () {
+  gulp.src(['./js/envii.js', './js/!(envii)*.js'])
+    .pipe(concat('envii.js'))
+    .pipe(gulp.dest('./test'));
+});
+
+
+gulp.task('dev', function() {
+  return watch(['./js/*.js'], function() {
+    gulp.run(['bundle-js']);
+  });
+});
+
 gulp.task('default', ['build']);
+
+
